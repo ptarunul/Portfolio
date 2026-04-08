fddf472f242048ed6efec233c9a029053240ec39
Tarun193 <ptarun974@gmail.com>
2026-04-08 19:11:46 +0100

    remove testimonials

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -2,8 +2,7 @@
 
 import React from "react";
 
-import { companies, testimonials } from "@/data";
-import { InfiniteMovingCards } from "./ui/InfiniteCards";
+import { companies } from "@/data";
 
 const Clients = () => {
   return (
@@ -14,17 +13,6 @@ const Clients = () => {
       </h1>
 
       <div className="flex flex-col items-center max-lg:mt-10">
-        <div
-          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
-          className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
-        >
-          <InfiniteMovingCards
-            items={testimonials}
-            direction="right"
-            speed="slow"
-          />
-        </div>
-
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map((company) => (
             <React.Fragment key={company.id}>
